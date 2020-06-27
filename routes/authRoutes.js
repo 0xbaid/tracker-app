c8a0fff331b840db609df1329738d8407c6a5baa
Obaid <obaidulhaq20@gmail.com>
2020-06-27 19:46:52 +0500

    added user in db

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 // @route Post /signup
 // @desc signup user
 // @access public
-router.post('/signup', (req, res) => {
-  console.log(req.body);
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+  await user.save();
+  
   res.send('You made a post request');
 });
 

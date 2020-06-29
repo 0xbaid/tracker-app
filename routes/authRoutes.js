a6d9b628abae8bd86ff31cab27c34f13d8b474d0
Obaid <obaidulhaq20@gmail.com>
2020-06-30 04:17:57 +0500

    hashed and compared password by bcrypt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,12 +11,34 @@ router.post('/signup', async (req, res) => {
   try {
     const user = new User({ email, password });
     await user.save();
-    
+
     //creating token
-    const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY')
-    res.send({token});
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
   } catch (error) {
     return res.status(422).send(error.message);
+  }
+});
+
+// @route Post /signin
+// @desc signin user
+// @access private
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(422).send({ error: 'Invalid email or password' });
+    }
+
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid email or password' });
   }
 });
 

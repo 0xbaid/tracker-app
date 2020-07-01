636ff1b2d5c2cf994098d42419bfe991d924759c
Obaid <obaidulhaq20@gmail.com>
2020-07-01 23:29:24 +0500

    added route to post tracks

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Track = require('../models/Track');
 const auth = require('../middleware/auth');
 
-// @route Post /track
+// @route Get /track
 // @desc get track data
 // @access private
 router.get('/tracks', auth, async (req, res) => {
@@ -12,6 +12,25 @@ router.get('/tracks', auth, async (req, res) => {
     res.send(tracks);
   } catch (err) {
     return res.status(422).send({ error: 'No tracks found' });
+  }
+});
+
+// @route Post /track
+// @desc post track data
+// @access private
+router.post('/tracks', auth, async (req, res) => {
+  const { name, locations } = req.body;
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide name and location' });
+  }
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    return res.status(422).send({error: err.message});
   }
 });
 
